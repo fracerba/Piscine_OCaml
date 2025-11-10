546e667e2a72d0e2adda287e78fd938a1436a0eb
Francesco Cerbai <1ecerbaifrancesco@gmail.com>
2025-11-10 12:28:36 +0100

    Renamed directories

--- OCaml_02/ex00/repeat_x.ml
+++ OCaml_02/ex00/repeat_x.ml
@@ -1,13 +1,13 @@
 let rec repeat_x n =
 	if n < 0 then
 		"Error"
-	else if n > 0 then
-		"x" ^ repeat_x (n - 1)
-	else
+	else if n = 0 then
 		""
+	else
+		"x" ^ repeat_x (n - 1)
 
 let () =
-  print_endline (repeat_x (-1));
+	print_endline (repeat_x (-1));
 	print_endline (repeat_x 0);
 	print_endline (repeat_x 1);
 	print_endline (repeat_x 2);
