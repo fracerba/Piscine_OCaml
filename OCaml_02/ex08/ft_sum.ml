c272094eff559ccf020d2840b0d12b483c815596
Francesco Cerbai <1ecerbaifrancesco@gmail.com>
2025-11-13 12:56:34 +0100

    leibniz_pi

--- OCaml_02/ex08/ft_sum.ml
+++ OCaml_02/ex08/ft_sum.ml
@@ -1,12 +1,12 @@
 let ft_sum f l u =
-	let rec loop f l r =
+	let rec loop l r =
 		if l > u then
 			nan
 		else if l = u then
 			r +. f l
 		else
-			loop f (l + 1) (r +. (f l))
-		in loop f l 0.0
+			loop (l + 1) (r +. (f l))
+		in loop l 0.0
 
 let () =
 	print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10);
--- OCaml_02/ex09/leibniz_pi.ml
+++ OCaml_02/ex09/leibniz_pi.ml
@@ -1,19 +1,26 @@
 let leibniz_pi d =
-	let calc i =
-		if i mod 2 = 0 then 
-			1.0 /. float_of_int ((2 * i + 1))
-		else 
-			-1.0 /. float_of_int ((2 * i + 1))
-	in let rec loop n a =
-		if d < 0 then
+	let power i =
+		-1.0 ** float_of_int i
+	in let calc i =
+		4.0 *. (power i /. float_of_int ((2 * i + 1)))
+	in let diff a pi =
+		if pi > a then
+			pi -. a
+		else
+			a -. pi
+	in let rec loop n a pi =
+		if d < 0.0 then
 			-1
-		else if d > (4.0 *. atan 1.0) - a || d > a - (4.0 *. atan 1.0) then
+		else if d > diff a pi then
 			n
 		else
-			loop (n + 1) (a + calc (n + 1)) 
-	in loop 0 (4.0 *. calc 0)
+			loop (n + 1) (a +. calc (n + 1)) pi
+	in loop 0 (calc 0) (4.0 *. atan 1.0)
 
 let () =
+	print_int (leibniz_pi (-0.01));
+	print_char ('\n');
+
 	print_int (leibniz_pi 0.05);
 	print_char ('\n');
 
@@ -21,4 +28,7 @@ let () =
 	print_char ('\n');
 
 	print_int (leibniz_pi 0.1);
+	print_char ('\n');
+
+	print_int (leibniz_pi 0.000001);
 	print_char ('\n')
