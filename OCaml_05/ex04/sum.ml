09c6a48ac148ce4bf3b80932bf54b61a4c5d3f14
Francesco Cerbai <1ecerbaifrancesco@gmail.com>
2026-01-14 17:16:29 +0100

    Day 05 ex06

--- OCaml_05/ex04/sum.ml
+++ OCaml_05/ex04/sum.ml
@@ -7,4 +7,4 @@ let () =
 	Printf.printf "%.2f\n" (sum 10.87 31.12);
 	Printf.printf "%.4f\n" (sum 6748.7585 5638.5689);
 	Printf.printf "%.1f\n" (sum 10.87 nan);
-	Printf.printf "%.1f\n" (sum max_float (-.min_float))
+	Printf.printf "%.1f\n" (sum max_float (-. min_float))
---
+++ OCaml_05/ex06/examples_of_file.ml
@@ -0,0 +1,38 @@
+let examples_of_file (file : string) : (float array * string) list =
+	let rec create_tuple l a : (float array * string) =
+		match l with
+			| h :: n :: t -> create_tuple (n :: t) (Array.append a [|float_of_string h|])
+			| h :: t -> (a, t)
+	in try
+		let filein = 
+			open_in file
+		in let rec create_list l = 
+			try
+				(create_tuple (String.split_on_char ',' (input_line filein)) [||]) :: (create_list l)
+			with End_of_file -> close_in filein;
+				l
+		in create_list []
+	with Sys_error err -> print_endline	("Error: " ^ err);
+		[]
+
+let () =
+	let print_array (a : float array) =
+		print_string "([|";
+		for i = 0 to (Array.length a) - 2 do begin
+			print_float (Array.get a i);
+			print_string "; ";
+		end
+		print_float (Array.get a ((Array.length a) - 1));
+		print_string "|], ";
+	in let print_example (e : (float array * string)) =
+		print_array (fst e);
+		print_endline "\"" ^ (snd e) ^ "\")"
+	in let print_examples_list (l : (float array * string) list) =
+		match l with
+			| h :: t -> print_example h;
+									print_examples_list t
+			| _ -> ()
+	in if Array.length Sys.argv = 2 then
+		print_examples_list (examples_of_file Sys.argv.(1))
+	else
+		print_endline	"Error: invalid arguments"
