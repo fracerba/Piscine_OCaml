36e99aee4af969b5e703b1ebd395c1f3f9ad6144
francesco <1ecerbaifrancesco@gmail.com>
2026-02-23 14:57:58 +0100

    OCaml 07 ex00

---
+++ OCaml_07/ex00/main.ml
@@ -0,0 +1,9 @@
+let () =
+  let people1 = new People.people "Rory Williams" in
+  let people2 = new People.people "Amy Pond" in
+  print_endline people1#to_string;
+  print_endline people2#to_string;
+  people1#talk;
+  people2#talk;
+  people1#die;
+  people2#die
