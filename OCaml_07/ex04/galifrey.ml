200386c79fcf64704ca4acd3de7da3a56582466d
francesco <1ecerbaifrancesco@gmail.com>
2026-04-30 15:52:27 +0200

    Finished Piscine OCaml

--- OCaml_07/ex04/galifrey.ml
+++ OCaml_07/ex04/galifrey.ml
@@ -4,27 +4,10 @@ class galifrey (peoples : People.people list) (doctors : Doctor.doctor list) (da
 		val doctors : Doctor.doctor list = doctors
 		val people : People.people list = peoples
 
-		method private list_iter f lst =
-			List.iter f (List.rev lst#get_members);
-			print_newline ()
-
-		method private extract_shield (d : Dalek.dalek) =
+		method private extract_shield (d : Dalek.dalek) : bool =
 			let s = d#to_string in
 			let ridx = String.rindex s ':' in
 			bool_of_string (String.sub s (ridx + 2) (String.length s - ridx - 2))
-
-		method private build_army lst species : 'a Army.army =
-			let extract_name c =
-				let s = c#to_string in
-				let idx = String.index s ':' in
-				String.sub s (idx + 2) (String.index s '|' - idx - 3)
-			in let print h =
-				print_endline (extract_name h ^ " has joined the " ^ species ^ " army!");
-			in let rec loop army lst =
-				match lst with
-					| [] -> (print_newline (); army)
-					| h :: t -> (print h; loop (army#add h) t)
-			in loop (new Army.army []) lst
 
 		method private build_people_army lst species : People.people Army.army =
 			let extract_name c =
@@ -88,7 +71,7 @@ class galifrey (peoples : People.people list) (doctors : Doctor.doctor list) (da
 				else
 					lst
 			in let rec loop lst n =
-				if n < 0 then begin
+				if n < 0 || lst = [] then begin
 					print_newline ();
 					recreate_army lst
 				end
@@ -105,15 +88,15 @@ class galifrey (peoples : People.people list) (doctors : Doctor.doctor list) (da
 				else
 					d
 			in let damage_doctors lst =
+				print_endline ("Exterminate!");
 				if Random.bool () then begin
 					let doc = List.nth lst (Random.int (List.length lst)) in
-					print_endline ("Exterminate!");
 					List.map (damage doc) lst
 				end
 				else
 					lst
 			in let rec loop lst n =
-				if n < 0 then begin
+				if n < 0 || lst = [] then begin
 					print_newline ();
 					recreate_army lst
 				end
@@ -125,17 +108,17 @@ class galifrey (peoples : People.people list) (doctors : Doctor.doctor list) (da
 			let recreate_army lst =
 				List.fold_left (fun acc p -> acc#add p) (new Army.army []) lst
 			in let exterminate_people n lst =
+				print_endline ("Exterminate!");
 				if Random.bool () then begin
 					let p = List.nth lst (Random.int (List.length lst)) in
 					let dal = List.nth daleks#get_members n in
-					print_endline ("Exterminate!");
 					dal#exterminate p;
 					List.filter (fun x -> x#to_string <> p#to_string) lst
 				end
 				else
 					lst
 			in let rec loop lst n =
-				if n < 0 then begin
+				if n < 0 || lst = [] then begin
 					print_newline ();
 					recreate_army lst
 				end
@@ -143,30 +126,47 @@ class galifrey (peoples : People.people list) (doctors : Doctor.doctor list) (da
 					loop (exterminate_people n lst) (n - 1)
 			in loop (List.rev peoples#get_members) ((List.length daleks#get_members) - 1)
 			
-		method private check_alive (entities : 'a Army.army) =
+		method private check_people (entities : People.people Army.army) =
+			List.length entities#get_members <> 0
+
+		method private check_doctors (entities : Doctor.doctor Army.army) =
+			List.length entities#get_members <> 0
+		
+		method private check_daleks (entities : Dalek.dalek Army.army) =
 			List.length entities#get_members <> 0
 
 		method private print_armies (peoples : People.people Army.army) (doctors : Doctor.doctor Army.army) (daleks : Dalek.dalek Army.army) =
-			print_endline "Peoples:";
-			self#list_iter (fun p -> print_endline (p#to_string)) peoples;
-			print_endline "Doctors:";
-			self#list_iter (fun d -> print_endline (d#to_string)) doctors;
-			print_endline "Daleks:";
-			self#list_iter (fun d -> print_endline (d#to_string)) daleks
+			let print_species species lst =
+				if lst <> [] then begin
+					print_endline (species ^ ":");
+					List.iter (fun p -> print_endline (p#to_string)) lst;
+					print_newline ()
+				end
+			in let people_list = peoples#get_members in
+			let doctor_list = doctors#get_members in
+			let dalek_list = daleks#get_members in
+
+			if people_list <> [] then
+				print_species "Peoples" people_list;
+			if doctor_list <> [] then
+				print_species "Doctors" doctor_list;
+			if dalek_list <> [] then
+				print_species "Daleks" dalek_list;
 
 		method private simulate_war (peoples : People.people Army.army) (doctors : Doctor.doctor Army.army) (daleks : Dalek.dalek Army.army) =
-			if not (self#check_alive peoples) || not (self#check_alive doctors) then
+			self#print_armies peoples doctors daleks;
+			if not (self#check_people peoples) || not (self#check_doctors doctors) then
 				print_endline "The Daleks have won the Time War!"
-			else if not (self#check_alive daleks) then
+			else if not (self#check_daleks daleks) then
 				print_endline "The Doctors and Peoples have won the Time War!"
 			else begin
-				self#print_armies peoples doctors daleks;
-				if Random.bool () then begin
+				let shield = List.for_all self#extract_shield daleks#get_members in
+				if Random.bool () && not shield then begin
 					print_endline "The Doctors are attacking the Daleks!";
 					self#simulate_war peoples doctors (self#attack_daleks doctors daleks)
 				end
 				else
-					if Random.bool () then begin
+					if Random.bool () && not shield then begin
 						print_endline "The Daleks are attacking the Doctors!";
 						self#simulate_war peoples (self#damage_doctors_army doctors daleks) daleks 
 					end 
@@ -180,6 +180,6 @@ class galifrey (peoples : People.people list) (doctors : Doctor.doctor list) (da
 			let peoples_army = self#build_people_army people "people" in
 			let doctors_army = self#build_doctors_army doctors "doctors" in
 			let daleks_army = self#build_daleks_army daleks "daleks" in
-			print_endline "The Time War has begun!";
+			print_endline "THE TIME WAR HAS BEGUN!\n";
 			self#simulate_war peoples_army doctors_army daleks_army
 end
--- OCaml_07/ex04/main.ml
+++ OCaml_07/ex04/main.ml
@@ -2,20 +2,22 @@ let init_armies peoples doctors daleks =
 	let list_init lst f =
 		List.init (List.length lst) (fun i -> f i)
 	in let peoples_fun i = new People.people (List.nth peoples i) in
-	let peoples = list_init peoples peoples_fun in
+	let new_peoples = list_init peoples peoples_fun in
 	print_newline ();
 
-	let doctors_fun i = new Doctor.doctor (List.nth doctors i) (1200 + i * 300) (List.nth peoples i) in
-	let doctors = list_init doctors doctors_fun in
+	let doctors_fun i = new Doctor.doctor (List.nth doctors i) (1000 + i * 300) (List.nth new_peoples i) in
+	let new_doctors = list_init doctors doctors_fun in
 	print_newline ();
 
-	let daleks = List.init 5 (fun _ -> new Dalek.dalek) in
-	(peoples, doctors, daleks)
+	let new_daleks = List.init daleks (fun _ -> new Dalek.dalek) in
+	(new_peoples, new_doctors, new_daleks)
 
 let () = 
-	let peoples = ["Donna Noble"; "Amy Pond"; "Rory Williams"; "Rose Tyler"; "Clara Oswald";]
-	and doctors = ["The Ninth Doctor"; "The Tenth Doctor"; "The Eleventh Doctor";]
-	and dalek = 5 in
+	let peoples = ["Donna Noble"; "Amy Pond"; "Rory Williams"; "Rose Tyler"; "Clara Oswald";
+		"Susan Foreman"; "Barbara Wright"; "Ian Chesterton"; "Steven Taylor";
+		"Dodo Chaplet"; "Polly Wright"; "Ben Jackson"; "Jamie McCrimmon"; "Victoria Waterfield";]
+	and doctors = ["The War Doctor"; "The Ninth Doctor"; "The Tenth Doctor"; "The Eleventh Doctor";]
+	and dalek = 7 in
 
 	let peoples, doctors, daleks = init_armies peoples doctors dalek in
 	let galifrey = new Galifrey.galifrey peoples doctors daleks in
